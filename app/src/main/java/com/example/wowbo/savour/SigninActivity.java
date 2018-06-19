b28061192d72ffd96834c494c72700dba1bdf755
wowbob396 <wowbob396@gmail.com>
2018-06-18 20:30:43 -0500

    added project files

---
+++ app/src/main/java/com/example/wowbo/savour/SigninActivity.java
@@ -0,0 +1,11 @@
+package com.example.wowbo.savour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
